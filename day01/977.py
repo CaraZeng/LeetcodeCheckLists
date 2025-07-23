2a5b7981534145b532f19c92ddb399ac0d577a4a
cara <carazengca@gmail.com>
2025-07-23 22:40:00 +0800

    detail

--- day01/977.py
+++ day01/977.py
@@ -1,4 +1,11 @@
 # Two Pointers from start and end to mid
+# The value of the numbers' square will be bigger on the left and right side 
+# and smaller in the middle, since there -4's square will be 16
+# we can create a new List which is the same length, and put the pointer at the 
+# last one element. Then we put two pointers at first and last one element in 
+# the old list and compare their square. Then we put the bigger one into the new
+# List from its end.
+
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         i = 0
