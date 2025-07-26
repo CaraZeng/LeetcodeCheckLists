41ef5bffae43b897c60e23e485d90d4e807b01b2
cara <carazengca@gmail.com>
2025-07-26 21:58:38 +0800

    Update

--- day04/142.py
+++ day04/142.py
@@ -6,6 +6,13 @@
 # 3. Why cant just while fast? because if its an empty Linked List and we call
 # fast.next, it would raise an error cause right now fast is None and None cant
 # has a next.
+# 4. Why slow and fast would meet at the first loop of slow?
+# Since the speed difference between slow and fast is 1, so the fast wont 
+# jump over slow, it would meets slow once it arrives at slow spot.
+# And we can know that during slow's first loop, even though fast is one loop 
+# behind slow(which is the extreme circumstances because if its one loop away
+# that means they meet), since its speed is twice the slow's, its route would
+# cover slow's.
 
 # Definition for singly-linked list.
 # class ListNode:
