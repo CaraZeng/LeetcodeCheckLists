e919186f4791d2e32f4e6786ddb66a790d53fe5c
cara <carazengca@gmail.com>
2025-08-26 16:16:14 +0800

    Update day17

---
+++ day17/617.py
@@ -0,0 +1,23 @@
+# merge two binary trees (recursion)
+
+# Key Note:
+# 1. if there are no root1, then we need root2's value to be the value of new tree(in this case we use
+# the old tree1 and update it to be the new tree.)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        return root1
